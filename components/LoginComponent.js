eb009595cd14ffe718c2d83dff0d583b8de4177f
dark-theme-programmer <abdulahad.oct99@gmail.com>
2020-04-29 06:41:23 +0500

    Image Manipulator

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -5,6 +5,7 @@ import { baseUrl } from '../shared/baseUrl';
 import * as Permissions from 'expo-permissions';
 import * as SecureStore from 'expo-secure-store';
 import * as ImagePicker from 'expo-image-picker';
+import * as ImageManipulator from 'expo-image-manipulator';
 import { createBottomTabNavigator } from 'react-navigation';
 
 
@@ -148,9 +149,22 @@ class RegisterTab extends Component {
             });
             if(!capturedImage.cancelled) {
                 console.log(capturedImage);
-                this.setState({imageUrl: capturedImage.uri });
+                this.processImage(capturedImage.uri );
             }
         }
+    }
+
+    processImage = async (imageUri) => {
+        let processedImage = await ImageManipulator.manipulateAsync(
+            imageUri, 
+            [
+                {resize: {width: 400}}
+            ],
+            {format: 'png'}
+        );
+        console.log(processedImage);
+        this.setState({imageUrl: processedImage.uri });
+
     }
 
     static navigationOptions = {
